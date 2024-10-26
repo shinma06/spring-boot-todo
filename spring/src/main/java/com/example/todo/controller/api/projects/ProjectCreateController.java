e1045d1d66b00cc2dc18d9b8736eb1db556ff17f
shinma06 <shinma.0103.2006@gmail.com>
2024-10-26 11:12:31 +0900

    add: ProjectCreateController を本実装

--- spring/src/main/java/com/example/todo/controller/api/projects/ProjectCreateController.java
+++ spring/src/main/java/com/example/todo/controller/api/projects/ProjectCreateController.java
@@ -1,4 +1,5 @@
 package com.example.todo.controller.api.projects;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -6,14 +7,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.todo.dto.request.projects.ProjectCreateRequest;
 import com.example.todo.entity.Project;
+import com.example.todo.service.projects.ProjectCreateService;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/projects")
 public class ProjectCreateController {
+
+  private final ProjectCreateService projectCreateService;
+
+  public ProjectCreateController(ProjectCreateService projectCreateService) {
+    this.projectCreateService = projectCreateService;
+  }
+
   @PostMapping
   public ResponseEntity<Project> invoke(@Valid @RequestBody ProjectCreateRequest request) {
-    Project project = new Project();
+    Project project = this.projectCreateService.invoke(request);
+
     return ResponseEntity.ok(project);
   }
 }
