9d24491ffc08ac53fb1b2f0f3ada56cd6faa7fc8
shinma06 <shinma.0103.2006@gmail.com>
2024-10-26 11:13:57 +0900

    add: created_at と updated_at の自動設定処理を追加

--- spring/src/main/java/com/example/todo/entity/Project.java
+++ spring/src/main/java/com/example/todo/entity/Project.java
@@ -1,10 +1,13 @@
 package com.example.todo.entity;
+
 import java.time.LocalDateTime;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -12,17 +15,35 @@ import lombok.Data;
 @Entity
 @Table(name = "projects")
 public class Project {
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
+
   @Column(nullable = false, length = 255)
   private String name;
+
   @Column(columnDefinition = "TEXT")
   private String summary;
+
   @Column(name = "deleted_at")
   private LocalDateTime deletedAt;
+
   @Column(name = "created_at", nullable = false)
   private LocalDateTime createdAt;
+
   @Column(name = "updated_at", nullable = false)
   private LocalDateTime updatedAt;
+
+  @PrePersist
+  private void onCreate() {
+    LocalDateTime now = LocalDateTime.now();
+    this.setCreatedAt(now);
+    this.setUpdatedAt(now);
+  }
+
+  @PreUpdate
+  private void onUpdate() {
+    this.setUpdatedAt(LocalDateTime.now());
+  }
 }
