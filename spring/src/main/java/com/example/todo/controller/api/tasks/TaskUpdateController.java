7abe9eade210c5134ef7022b7d8f79306967fc6b
shinma06 <shinma.0103.2006@gmail.com>
2025-01-25 18:01:18 +0900

    add: Controller の実装
      - Service も定義のみ実装

--- spring/src/main/java/com/example/todo/controller/api/tasks/TaskUpdateController.java
+++ spring/src/main/java/com/example/todo/controller/api/tasks/TaskUpdateController.java
@@ -1,7 +1,6 @@
 package com.example.todo.controller.api.tasks;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,9 +19,9 @@ public class TaskUpdateController {
     this.taskUpdateService = taskUpdateService;
   }
 
-  @PutMapping
   public ResponseEntity<TaskBaseResponse> invoke(@Valid @RequestBody TaskUpdateRequest request) {
     TaskBaseResponse response = this.taskUpdateService.invoke(request);
+
     return ResponseEntity.ok(response);
   }
 }
