bc9e6064084013a972d65aefb3f5fb8d34a9a7a1
shinma06 <shinma.0103.2006@gmail.com>
2025-01-25 17:41:14 +0900

    add: Controller の実装
      - Service も定義のみ実装

--- spring/src/main/java/com/example/todo/controller/api/tasks/TaskToggleController.java
+++ spring/src/main/java/com/example/todo/controller/api/tasks/TaskToggleController.java
@@ -1,13 +1,12 @@
 package com.example.todo.controller.api.tasks;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.todo.dto.response.tasks.TaskBaseResponse;
 import com.example.todo.service.tasks.TaskToggleService;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-
 
 @RestController
 @RequestMapping("/api/tasks")
@@ -19,9 +18,16 @@ public class TaskToggleController {
     this.taskToggleService = taskToggleService;
   }
 
+  /**
+   * 例: PUT /api/tasks/123/toggle というパスになる。
+   *
+   * @PathVariable パスの中に含まれる {} で囲まれた部分を変数として受け取ることができる。この場合、id という変数にパスの中に含まれる値が入る。
+   */
   @PutMapping("/{id}/toggle")
   public ResponseEntity<TaskBaseResponse> invoke(@PathVariable Integer id) {
-    TaskBaseResponse task = this.taskToggleService.invoke(id);
-    return ResponseEntity.ok(task);
+
+    TaskBaseResponse response = this.taskToggleService.invoke(id);
+
+    return ResponseEntity.ok(response);
   }
 }
