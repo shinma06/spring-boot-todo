ffe0c5d87a5143d6993f66f6aa255d1edd7eae94
shinma06 <shinma.0103.2006@gmail.com>
2025-01-25 16:17:34 +0900

    add: Controllerの実装
    - Service も定義のみ実装

--- spring/src/main/java/com/example/todo/controller/api/tasks/TaskSearchController.java
+++ spring/src/main/java/com/example/todo/controller/api/tasks/TaskSearchController.java
@@ -1,5 +1,6 @@
 package com.example.todo.controller.api.tasks;
 
+import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -8,8 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.todo.dto.request.tasks.TaskSearchRequest;
 import com.example.todo.dto.response.tasks.TaskBaseResponse;
 import com.example.todo.service.tasks.TaskSearchService;
-import jakarta.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/tasks")
@@ -24,11 +23,10 @@ public class TaskSearchController {
   /**
    * @ModelAttribute メソッドの引数に付与するアノテーション。リクエストパラメータを受け取るために使用する。
    *                 リクエストパラメータとは、HTTPリクエストのクエリパラメータやフォームパラメータのこと。
-   *                 例えば、`/api/tasks?status=xxx`のようなリクエストの status パラメータを受け取る場合に使用する。
+   *                 例えば、`/api/tasks?projectId=xxx`のようなリクエストの projectId パラメータを受け取る場合に使用する。
    */
   @GetMapping
-  public ResponseEntity<List<TaskBaseResponse>> invoke(
-      @Valid @ModelAttribute TaskSearchRequest request) {
+  public ResponseEntity<List<TaskBaseResponse>> invoke(@ModelAttribute TaskSearchRequest request) {
     List<TaskBaseResponse> tasks = this.taskSearchService.invoke(request);
 
     return ResponseEntity.ok(tasks);
